68ea92816b97e30977276a81f777a4069577c431
ajdahcode <90115717+ajdahcode@users.noreply.github.com>
2021-10-11 23:51:46 -0400

    Update AppTest.java

--- src/test/java/ex45/AppTest.java
+++ src/test/java/ex45/AppTest.java
@@ -2,12 +2,22 @@ package ex45;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
     @Test
-    void modifytext() {
+    void modifytext() throws IOException {
         App app = new App();
+        app.modifytext("test");
+        Path testPath = Paths.get("src/main/java/ex45/test.txt");
+        boolean exists = Files.exists(testPath);
+        assertTrue(exists);
+        if (exists) Files.delete(testPath);
     }
 }

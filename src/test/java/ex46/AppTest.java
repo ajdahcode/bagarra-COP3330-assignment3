d817ce875bd4becd81bb760c8f1fa3c754a546d2
ajdahcode <90115717+ajdahcode@users.noreply.github.com>
2021-10-11 23:59:31 -0400

    Update AppTest.java

--- src/test/java/ex46/AppTest.java
+++ src/test/java/ex46/AppTest.java
@@ -3,16 +3,26 @@ package ex46;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
     @Test
-    void freqFinder() throws Exception {
-        App app = new App();
+    void freqFinder() throws FileNotFoundException {
         File file = new File(
                 "src/test/java/ex46/exercise46_input.txt");
-        Scanner scn = new Scanner(file);
-        app.freqFinder(scn);
+        App app = new App();
+        Map<String,Integer> actual = app.freqFinder(new Scanner(file));
+        Map<String,Integer> expected = new HashMap<>();
+        expected.put("cats",1);
+        expected.put("dog",2);
+
+       assertEquals(expected,actual);
+
     }
 }

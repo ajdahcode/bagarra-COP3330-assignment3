7266b4c1462de5f3d1c3eadec0a8bfd8113c9887
ajdahcode <90115717+ajdahcode@users.noreply.github.com>
2021-10-11 23:59:52 -0400

    Update App.java

--- src/main/java/ex46/App.java
+++ src/main/java/ex46/App.java
@@ -9,33 +9,27 @@ import java.io.*;
 import java.util.stream.Collectors;
 
 public class App {
-
-    // Function for finding the frequency of a word
-    public static void freqFinder(Scanner scn){
+    // Function for finding the freqMapuency of a word
+    public static Map<String, Integer> freqFinder(Scanner scn){
         // Create a map
-        Map<String, Integer> freq = new HashMap<String, Integer>();
-
-        // Create an array
-        ArrayList<String> word = new ArrayList<String>();
+        Map<String, Integer> freqMap = new HashMap<String, Integer>();
 
         // Process of reading and adding the words into an array and map
         while (scn.hasNextLine()) {
             String data = scn.nextLine();
             String[] words = data.split(" ");
             for(String w : words){
-                // System.out.println(w);
-                word.add(w);
-                if(!freq.containsKey(w)){
-                    freq.put(w, 1);
+                if(!freqMap.containsKey(w)){
+                    freqMap.put(w, 1);
                 }
                 else{
-                    freq.put(w, freq.get(w)+1);
+                    freqMap.put(w, freqMap.get(w)+1);
                 }
             }
         }
 
-        // Process of putting the word frequency of highest to lowest
-        Map<String, Integer> sortFreq = freq.entrySet().stream()
+        // Process of putting the word freqMapuency of highest to lowest
+        Map<String, Integer> sortedfreqMap = freqMap.entrySet().stream()
                 .sorted(Comparator.comparingInt(e -> -e.getValue()))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
@@ -44,8 +38,12 @@ public class App {
                         LinkedHashMap::new
                 ));
 
-        // Print out the word and their frequency by using '*'
-        for (Map.Entry<String, Integer> entry : sortFreq.entrySet()) {
+        return sortedfreqMap;
+    }
+
+    public static void printMap(Map<String,Integer> map) {
+        // Print out the word and their freqMapuency by using '*'
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.printf("%-11s", entry.getKey() + ":");
 
             for(int i = 0; i < entry.getValue(); i++){
@@ -54,13 +52,15 @@ public class App {
             System.out.println();
         }
     }
+
     public static void main( String[] args ) throws Exception {
         // Open the file and take in the strings
         File file = new File(
                 "src/main/java/ex46/exercise46_input.txt");
         Scanner scn = new Scanner(file);
 
-        // Function used to find the frequency of the words
-        freqFinder(scn);
+        // Function used to find the freqMapuency of the words
+        Map<String, Integer> wordMap = freqFinder(scn);
+        printMap(wordMap);
     }
 }
